bdb84d6dc71e48517dc6b5fe48b6ac86d35aad8d
Kiker01 <kakycha@gmail.com>
2019-04-05 13:29:59 +0300

    tracers reload

---
+++ addons/sg_rhs_ace_tracers/config.cpp
@@ -0,0 +1,28 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ace_tracers
+	{
+		name="RHS - Tracers";
+		units[]={};
+		weapons[]={};
+		requiredVersion=1.88;
+		requiredAddons[]=
+		{
+			"ace_ballistics"
+		};
+		authors[]=
+		{
+			"SG Team"
+		};
+		version="3.12.6.43";
+		versionStr="3.12.6.43";
+		versionAr[]={3,12,6,43};
+		magazines[]={};
+		ammo[]=
+		{};
+	};
+};
+#include "CfgAmmo.hpp"
+#include "CfgMagazines.hpp"
